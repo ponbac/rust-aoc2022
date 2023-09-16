dd56992c0eda50818533f1e9e06c04e38d397b55
ponbac <root@ponbac.xyz>
2023-09-16 15:09:18 +0200

    day 5 shell

---
+++ day5/src/main.rs
@@ -0,0 +1,21 @@
+static EXAMPLE_INPUT: &str = r#"
+[D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"#;
+
+fn main() {
+    let _input = include_str!("input.txt");
+
+    part1(EXAMPLE_INPUT);
+}
+
+fn part1(input: &str) {
+    todo!()
+}
