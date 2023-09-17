6888194ba94e742924d723e53002de8752a2747f
ponbac <root@ponbac.xyz>
2023-09-17 12:18:02 +0200

    day 7 shell

---
+++ day7/src/main.rs
@@ -0,0 +1,32 @@
+static EXAMPLE_INPUT: &str = r#"
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+"#;
+
+fn main() {
+    let input = include_str!("input.txt");
+    let input = EXAMPLE_INPUT;
+
+    println!("Hello, world!");
+}
