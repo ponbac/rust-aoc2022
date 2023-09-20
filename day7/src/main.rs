9eb44c9b770d88a35d73795d29dfbec3441bf5c0
ponbac <root@ponbac.xyz>
2023-09-20 12:27:26 +0200

    push it

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,9 +2,9 @@ use camino::Utf8PathBuf;
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while1},
-    combinator::map,
-    sequence::preceded,
-    IResult,
+    combinator::{all_consuming, map},
+    sequence::{preceded, separated_pair},
+    Finish, IResult,
 };
 
 static EXAMPLE_INPUT: &str = r#"
@@ -49,20 +49,68 @@ fn parse_cd(i: &str) -> IResult<&str, Cd> {
 
 #[derive(Debug)]
 enum Command {
-    Ls(Ls),
-    Cd(Cd),
+    Ls,
+    Cd(Utf8PathBuf),
+}
+
+impl From<Ls> for Command {
+    fn from(_ls: Ls) -> Self {
+        Command::Ls
+    }
+}
+
+impl From<Cd> for Command {
+    fn from(cd: Cd) -> Self {
+        Command::Cd(cd.0)
+    }
 }
 
 fn parse_command(i: &str) -> IResult<&str, Command> {
     let (i, _) = tag("$ ")(i)?;
-    alt((map(parse_ls, Command::Ls), map(parse_cd, Command::Cd)))(i)
+    alt((map(parse_ls, Into::into), map(parse_cd, Into::into)))(i)
+}
+
+#[derive(Debug)]
+enum Entry {
+    Dir(Utf8PathBuf),
+    File(u64, Utf8PathBuf),
+}
+
+fn parse_entry(i: &str) -> IResult<&str, Entry> {
+    let parse_file = map(
+        separated_pair(nom::character::complete::u64, tag(" "), parse_path),
+        |(size, path)| Entry::File(size, path),
+    );
+    let parse_dir = map(preceded(tag("dir "), parse_path), Entry::Dir);
+
+    alt((parse_file, parse_dir))(i)
+}
+
+#[derive(Debug)]
+enum Line {
+    Command(Command),
+    Entry(Entry),
+}
+
+fn parse_line(i: &str) -> IResult<&str, Line> {
+    alt((
+        map(parse_command, Line::Command),
+        map(parse_entry, Line::Entry),
+    ))(i)
 }
 
 fn main() {
     let input = include_str!("input.txt");
     let input = EXAMPLE_INPUT;
 
-    println!("Hello, world!");
+    let lines = input
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| all_consuming(parse_line)(l).finish().unwrap().1);
+
+    for line in lines {
+        println!("{line:?}");
+    }
 }
 
 fn parse_path(i: &str) -> IResult<&str, Utf8PathBuf> {
