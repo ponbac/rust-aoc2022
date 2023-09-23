2cf652eebe695267824cd8f5df91edb11ddd8aa5
ponbac <root@ponbac.xyz>
2023-09-23 10:35:29 +0200

    cleaner

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -3,14 +3,6 @@ use std::{
     fmt::{self, Debug, Formatter},
 };
 
-static EXAMPLE_INPUT: &str = r#"
-30373
-25512
-65332
-33549
-35390
-"#;
-
 #[derive(Debug, Hash, PartialEq, Eq)]
 enum Direction {
     Up,
@@ -124,7 +116,6 @@ impl TreeMap {
 
 fn main() {
     let input = include_str!("input.txt");
-    // let input = EXAMPLE_INPUT;
 
     let tree_map = TreeMap(
         input
@@ -160,7 +151,6 @@ fn main() {
             row.iter()
                 .enumerate()
                 .map(|(x, _)| tree_map.scenic_score(x, y))
-                // .inspect(|score| println!("{}: {}", y, score))
                 .max()
                 .unwrap()
         })
