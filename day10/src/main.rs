fdffd49a38017008e1a7323fa52cd412e9c0923b
ponbac <root@ponbac.xyz>
2023-09-24 15:06:46 +0200

    day 10 shell

---
+++ day10/src/main.rs
@@ -0,0 +1,8 @@
+static EXAMPLE_INPUT: &str = include_str!("example.txt");
+
+fn main() {
+    // let input = include_str!("input.txt");
+    let input = EXAMPLE_INPUT;
+
+    println!("Hello, world!");
+}
